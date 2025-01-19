cbb03de2815eed5b15b4370f97620bc7002828d3
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2025-01-19 12:21:10 -0300

    Updated controller

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -1,47 +1,49 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.AllArgsConstructor;
+
 @Controller
+@AllArgsConstructor
 public class TodoController {
 
-    private List<Todo> todos;
-
-    public TodoController() {
-        todos = new ArrayList<>();
-    }
+    private TodoRepository todos;
 
     @GetMapping("/")
     public String getTodos(Model memory) {
-        memory.addAttribute("todos", todos);
+        memory.addAttribute("todos", todos.findAll());
 
         return "/todo";
     }
 
     @PostMapping("/add") 
     public String createTodo(Todo aTodo) {
-        todos.add(aTodo);
+        todos.save(aTodo);
 
         return "redirect:/";
     }
 
     @GetMapping("/remove")
-    public String deleteTodo(@RequestParam String task) {
-        todos.removeIf(item -> item.getTask().equals(task));
+    public String deleteTodo(@RequestParam String id) {
+        todos.deleteById(id);
 
         return "redirect:/";
     }
 
     @GetMapping("/update")
-    public String updateTodo(@RequestParam String task) {
-        todos.stream().filter(item -> item.getTask().equals(task)).findFirst().ifPresent(todo -> todo.setStatus(!todo.isStatus()));
+    public String updateTodo(@RequestParam String id) {
+        var foundTodo = todos.findById(id);
+        
+        if (foundTodo.isPresent()) {
+            var aTodo = foundTodo.get();
+            aTodo.setStatus(!aTodo.isStatus());
+            todos.save(aTodo);
+        }
 
         return "redirect:/";
     }
