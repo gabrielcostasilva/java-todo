f305c208fb347e019af29aa842833e1bbe9d57f9
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2025-01-30 20:05:14 -0300

    Removed annotations

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -2,14 +2,10 @@ package com.example.demo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
 public class TodoController {
 
     private List<Todo> todos;
@@ -18,31 +14,25 @@ public class TodoController {
         todos = new ArrayList<>();
     }
 
-    @GetMapping("/")
-    public String getTodos(Model memory) {
-        memory.addAttribute("todos", todos);
-
-        return "/todo";
+    public List<Todo> getTodos() {
+        return todos;
     }
 
-    @PostMapping("/add") 
-    public String createTodo(Todo aTodo) {
+    public Todo createTodo(Todo aTodo) {
         todos.add(aTodo);
 
-        return "redirect:/";
+        return aTodo;
     }
 
-    @GetMapping("/remove")
-    public String deleteTodo(@RequestParam String id) {
+    public void deleteTodo(@RequestParam String id) {
         todos.removeIf(item -> item.getId().equals(id));
-
-        return "redirect:/";
     }
 
-    @GetMapping("/update")
-    public String updateTodo(@RequestParam String id) {
-        todos.stream().filter(item -> item.getId().equals(id)).findFirst().ifPresent(todo -> todo.setStatus(!todo.isStatus()));
+    public Optional<Todo> updateTodo(@RequestParam String id) {
+        Optional<Todo> aTodo = todos.stream().filter(item -> item.getId().equals(id)).findFirst();
 
-        return "redirect:/";
+        aTodo.ifPresent(todo -> todo.setStatus(!todo.isStatus()));
+
+        return aTodo;
     }
 }
