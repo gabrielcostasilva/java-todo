4a381a43a21dc7c1a0f64ae9af6d214729edfc7e
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2025-02-01 14:51:12 -0300

    Still tidying up ...

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,23 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@PostConstruct
-	public void init() {
-		TodoController controller = new TodoController();
-		controller.createTodo(new Todo("Lavar louça", false));
-		controller.createTodo(new Todo("Escovar dentes", true));
-		controller.createTodo(new Todo("Almoçar", false));
-
-		controller.getTodos().forEach(todo -> System.out.println(todo.getTask()));
 	}
 
 }
